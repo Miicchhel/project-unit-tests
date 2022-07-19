ffe98d23221c544b7595b7adce206be3f8968e39
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-07-19 17:36:22 -0300

    Implemente a função createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const objeto = {
+    name: `${nome}, o Lobo Solitário`, 
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return objeto;
+};
 
 module.exports = createStudent;
